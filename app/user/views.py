5c0ff05456abf39ab59e5f3fa493aa6cf67edf9a
Arij <aymen@tanitlab.com>
2021-11-16 16:34:53 +0100

    Add update and me api

--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from user.serializers import UserSerializers, AuthTokenSerializers
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
@@ -13,3 +13,14 @@ class CreateTokenView(ObtainAuthToken):
     """Creare a new auth token for user"""
     serializer_class = AuthTokenSerializers
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializers
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        return self.request.user
