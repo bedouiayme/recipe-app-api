cd63c0ec0e6305aa3bc1faf9d400184cbfa02fc9
Arij <aymen@tanitlab.com>
2021-11-17 14:35:13 +0100

    add tag model

---
+++ app/recipe/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from core.models import Tag
+from recipe import serializers
+
+
+class TagViewSet(viewsets.generics, mixins.ListModelMixin):
+    """Manage tags in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializers
